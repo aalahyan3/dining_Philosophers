56388d46e4aab18290ae2dd8fc2edccf3e1e7d82
aalahyan <me.a.alahyane@gmail.com>
2025-04-21 15:21:24 +0100

    philo updated

--- philo/philo_routine.c
+++ philo/philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:47:20 by aalahyan          #+#    #+#             */
-/*   Updated: 2025/04/21 14:09:43 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/04/21 15:04:56 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,15 +50,16 @@ void	print_log(t_philo *philo, char *message)
 {
 	long long	time;
 
+	time = get_time() - philo->data->start_time;
+	pthread_mutex_lock(&philo->data->print);
 	pthread_mutex_lock(&philo->data->stop_mutex);
 	if (philo->data->stop)
 	{
 		pthread_mutex_unlock(&philo->data->stop_mutex);
+		pthread_mutex_unlock(&philo->data->print);
 		return ;
 	}
 	pthread_mutex_unlock(&philo->data->stop_mutex);
-	time = get_time() - philo->data->start_time;
-	pthread_mutex_lock(&philo->data->print);
 	printf("--------------------------------------------------------------\n");
 	printf("| %-10lld | %-3d | %-40s|\n", time, philo->id, message);
 	pthread_mutex_unlock(&philo->data->print);
