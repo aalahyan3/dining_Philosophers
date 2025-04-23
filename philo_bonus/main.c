cb3891e11862ec46ca5f6a82b3b0d6018d1bfdf5
aalahyan <me.a.alahyane@gmail.com>
2025-04-23 15:54:19 +0100

    ddd

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -6,7 +6,7 @@
 /*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 18:18:03 by aalahyan          #+#    #+#             */
-/*   Updated: 2025/04/23 15:12:35 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/04/23 15:53:08 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,18 @@ int	init_data_2(t_data *data)
 	{
 		sem_close(data->forks_sem);
 		sem_close(data->print_sem);
-		sem_unlink(PRINT_SEMAPHORE);
-		sem_unlink(FORKS_SEMAPHORE);
 		return (0);
 	}
+	unlink(STOP_SEMAPHORE);
 	data->waiter_sem = sem_open(WAITER_SEMAPHORE, O_CREAT | O_EXCL, 0644, 1);
 	if (data->waiter_sem == SEM_FAILED)
 	{
 		sem_close(data->forks_sem);
 		sem_close(data->print_sem);
 		sem_close(data->stop_sem);
-		sem_unlink(STOP_SEMAPHORE);
-		sem_unlink(PRINT_SEMAPHORE);
-		sem_unlink(FORKS_SEMAPHORE);
 		return (0);
 	}
+	unlink(WAITER_SEMAPHORE);
 	data->start_time = get_time();
 	return (1);
 }
@@ -48,13 +45,14 @@ int	init_data(t_data *data)
 	O_CREAT | O_EXCL, 0644, data->nb_philo);
 	if (data->forks_sem == SEM_FAILED)
 		return (0);
+	unlink(FORKS_SEMAPHORE);
 	data->print_sem = sem_open(PRINT_SEMAPHORE, O_CREAT | O_EXCL, 0644, 1);
 	if (data->print_sem == SEM_FAILED)
 	{
 		sem_close(data->forks_sem);
-		sem_unlink(FORKS_SEMAPHORE);
 		return (0);
 	}
+	unlink(PRINT_SEMAPHORE);
 	return (init_data_2(data));
 }
 
