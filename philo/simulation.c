ab53084afd77c2a6f03b4a2a70ef68cae7fa4319
aalahyan <me.a.alahyane@gmail.com>
2025-04-26 09:59:00 +0100

    i have protected a mutex

--- philo/simulation.c
+++ philo/simulation.c
@@ -6,7 +6,7 @@
 /*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:15:21 by aalahyan          #+#    #+#             */
-/*   Updated: 2025/04/23 16:25:53 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/04/26 09:57:49 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,14 +44,19 @@ static int	get_left_fork(int id, int nb_p)
 		return (id - 2);
 }
 
-static void	init_philos(t_philo *philos, t_data *data)
+static int	init_philos(t_philo *philos, t_data *data)
 {
 	int	i;
 
 	i = 0;
 	while (i < data->nb_philo)
 	{
-		pthread_mutex_init(&philos[i].meal_mutex, NULL);
+		if (pthread_mutex_init(&philos[i].meal_mutex, NULL))
+		{
+			while (--i >= 0)
+				pthread_mutex_destroy(&philos[i].meal_mutex);
+			return (0);
+		}
 		philos[i].data = data;
 		philos[i].id = i + 1;
 		philos[i].last_meal = get_time();
@@ -60,6 +65,7 @@ static void	init_philos(t_philo *philos, t_data *data)
 		philos[i].nb_eat = 0;
 		i++;
 	}
+	return (1);
 }
 
 static void	start_threads(t_philo *philos, t_data *data)
@@ -96,7 +102,11 @@ void	start_simulation(t_data *data)
 	philos = malloc(data->nb_philo * sizeof(t_philo));
 	if (!philos)
 		return ;
-	init_philos(philos, data);
+	if (!init_philos(philos, data))
+	{
+		free(philos);
+		return ;
+	}
 	printf("--------------------------------------------------------------\n");
 	printf("| %-10s | %-3s | %-40s|\n", "time (ms)", "id", "logs");
 	data->start_time = get_time();
